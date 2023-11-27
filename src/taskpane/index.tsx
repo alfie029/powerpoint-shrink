b6a7d28b2b90334a3426494cae887e550c7d76bc
Alfie Zhang (Zhang Wenqing) <89895803+alfie029@users.noreply.github.com>
2023-11-27 15:37:12 +0800

    Initial commit with default PowerPoint plugin template

---
+++ src/taskpane/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+
+/* global document, Office, module, require, console */
+
+const title = "Seismic PowerPoint Shrinking Tool";
+
+const rootElement: HTMLElement = document.getElementById("container");
+const root = createRoot(rootElement);
+
+/* Render application after Office initializes */
+Office.onReady(() => {
+  root.render(
+    <FluentProvider theme={webLightTheme}>
+      <App title={title} />
+    </FluentProvider>
+  );
+}).then((r) => console.log(`${r}`));
+
+if ((module as any).hot) {
+  (module as any).hot.accept("./components/App", () => {
+    const NextApp = require("./components/App").default;
+    root.render(NextApp);
+  });
+}
